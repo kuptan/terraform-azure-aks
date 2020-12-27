6d915260be7aaa87377630883443291a7cc201a2
ibraheem saady <ibraheem@ibraheems-MacBook-Pro.local>
2020-12-27 18:46:59 +0100

    added sensitive on client_id and client_secret vars

--- variables.tf
+++ variables.tf
@@ -2,11 +2,13 @@
 
 variable "client_id" {
   description = "Service principal's client ID for AKS service principal configuration"
+  sensitive = true
   type = string
 }
 
 variable "client_secret" {
   description = "Service principal's client secret for AKS service principal configuration"
+  sensitive = true
   type = string
 }
 
